46b6c7f45500d1ff6783734b2ebc0e7f43b6ece3
Stack Frames <>
2023-05-16 00:53:06 +0530

    Added Search Demo

--- src/com/sf/gandalf/algorithms/search/SearchAlgorithms.java
+++ src/com/sf/gandalf/algorithms/search/SearchAlgorithms.java
@@ -1,4 +1,39 @@
 package com.sf.gandalf.algorithms.search;
 
-public class Search {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SearchAlgorithms {
+
+    static List<String> algorithms = new ArrayList<>();
+
+    static {
+        algorithms.add("1. Linear Search");
+        algorithms.add("2. Binary Search");
+    }
+
+    static int list[] = new int[]{2, 7, 9, 0, 6, 18, 5, 3};
+
+    public static void main(String[] args) {
+
+        algorithms.stream().forEach(System.out::println);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Select a Search Algorithm : \n");
+        int selection = scanner.nextInt();
+
+        switch (selection) {
+            case 01:
+                System.out.println("Linear Search");
+
+                System.out.println(LinearSearch.search(list, 18));
+
+                System.out.println("Recursive :" +LinearSearch.search(list, list.length, 18));
+                break;
+            default:
+                System.out.println("Invalid Selection.");
+        }
+
+    }
 }
