963bf6e8b078207214327e51750746c6759d3700
Stack Frames <>
2023-06-22 01:05:27 +0530

    Added Quick Sort Algorithm

---
+++ src/com/sf/gandalf/algorithms/sort/QuickSort.java
@@ -0,0 +1,9 @@
+package com.sf.gandalf.algorithms.sort;
+
+public class QuickSort {
+
+    public static int[] sort(int[] list) {
+        System.out.println("Quick Sort! - WIP");
+        return list;
+    }
+}
